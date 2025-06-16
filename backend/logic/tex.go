47c18c63e13805ff2b2709156faf54ec8df95bc0
yuuhikaze <yuuhikaze@protonmail.com>
2025-06-16 06:11:26 -0500

    feat: crops to diagram size

--- backend/logic/tex.go
+++ backend/logic/tex.go
@@ -12,7 +12,7 @@ func DotToTex(dot string) (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	cmd := exec.CommandContext(ctx, "dot2tex", "-ftikz")
+	cmd := exec.CommandContext(ctx, "dot2tex", "--crop", "-ftikz")
 	cmd.Stdin = bytes.NewBufferString(dot)
 
 	var out bytes.Buffer
