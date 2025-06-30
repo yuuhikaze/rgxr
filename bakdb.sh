e8ecde31eb59c66e75f64accfd4f1f77e27f5c0c
yuuhikaze <yuuhikaze@protonmail.com>
2025-06-30 11:02:47 -0500

    feat: adds convenience script to backup database

---
+++ bakdb.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -uo pipefail
+IFS=$'\n'
+
+docker exec -u postgres rgxr-postgres-1 \
+  pg_dump -d postgres -t api.finite_automatas -F c -f /tmp/finite_automatas.dump
+docker cp rgxr-postgres-1:/tmp/finite_automatas.dump ../finite_automatas.dump
