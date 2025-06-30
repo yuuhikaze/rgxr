afa37423efef541be27efe528376d56a1658c5ab
yuuhikaze <yuuhikaze@protonmail.com>
2025-06-30 11:03:29 -0500

    fix: makes backup scripts save dump as sql rather than binary

--- bakdb.sh
+++ bakdb.sh
@@ -4,5 +4,5 @@ set -uo pipefail
 IFS=$'\n'
 
 docker exec -u postgres rgxr-postgres-1 \
-  pg_dump -d postgres -t api.finite_automatas -F c -f /tmp/finite_automatas.dump
+  pg_dump -d postgres -t api.finite_automatas -f /tmp/finite_automatas.dump
 docker cp rgxr-postgres-1:/tmp/finite_automatas.dump ../finite_automatas.dump
